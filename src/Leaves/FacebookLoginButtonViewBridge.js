1163941465ec2f543b39b5b5806d313e667641a8
gcdneal <nmcconville@gcdtech.com>
2018-07-10 12:10:51 +0100

    updating view bridge to process a response from the social login scaffold and redirect.

--- src/Leaves/FacebookLoginButtonViewBridge.js
+++ src/Leaves/FacebookLoginButtonViewBridge.js
@@ -9,16 +9,18 @@ rhubarb.vb.create('FacebookLoginButtonViewBridge', function () {
       facebookLoginBtn.addEventListener('click', self.updateLoginStatus)
     },
     updateLoginStatus: function () {
-      debugger;
       let self = this;
       FB.login(function (response) {
         if (response.status === 'connected') {
-          debugger; //TODO check if we need to use the AUTH TOKEN from the response, i dont think the userID is the token we need to pass to the php side fb sdk
           this.access_token = response.authResponse.accessToken;
           FB.api('/me', {locale: 'en_US',  fields: 'first_name, last_name, email'}, function (userInfo) {
-            debugger;
             userInfo.access_token = this.access_token;
-            self.viewBridge.raiseServerEvent('attemptSocialLogin', userInfo,function(){console.log("pass")},function(){console.log("fail")})
+            self.viewBridge.raiseServerEvent('attemptSocialLogin', userInfo,
+            function(response){
+               window.location.href = response.redirectionUrl;
+            },function(){
+              //
+            })
               
           })
         }
